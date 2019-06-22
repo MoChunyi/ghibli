52c3dcedac9db20cab88abff7d96896ceedbb25e
mochunyi <mcyatom@gmail.com>
2019-06-22 17:09:25 +0800

    Updates

---
+++ precache-manifest.b1d60eedf30a4dbfa64ff443b0e0a694.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ff8a18dd8ddd18eccda4b675eccad7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45d89e6bd8aa1f147353",
+    "url": "./static/css/main.23561bc1.chunk.css"
+  },
+  {
+    "revision": "12d4f41936e7cb5f6b64",
+    "url": "./static/js/2.47d5459d.chunk.js"
+  },
+  {
+    "revision": "45d89e6bd8aa1f147353",
+    "url": "./static/js/main.e4d74ecd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "285ea0dba9d8c61ea8b2c5646fa600d8",
+    "url": "./static/media/totoro-run.285ea0db.gif"
+  },
+  {
+    "revision": "a6a7990b1320f209dee74aabf430a1f9",
+    "url": "./static/media/totoro-white-background.a6a7990b.jpg"
+  },
+  {
+    "revision": "a04e0472f394a5ff529563e16f0afbfb",
+    "url": "./static/media/totorologo.a04e0472.svg"
+  }
+]);
